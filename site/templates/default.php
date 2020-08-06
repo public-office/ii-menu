f6d8590e9869e6bc42a66b50dd68894a0c711e70
Paul Mylecharane <hi@public-office.info>
2020-08-06 13:56:58 +1000

    basics from class, Thurs 6th

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,5 +1,32 @@
-<? snippet('header') ?>
+<?php snippet('header') ?>
 
-    <h1><?= $page->title() ?></h1>
+  <nav>
+    <div class="works-texts">
+      <ul>
+        <? $works = page('works-texts') ?>
+        <?= $works->title() ?>
+        <? foreach(page('works-texts')->children() as $single): ?>
+          <li><a href="<?= $single->url() ?>"><?= $single->title() ?>, <?= $single->contributor()->toPage()->title() ?></a></li>
+        <? endforeach ?>
+      </ul>
+    </div>
 
-<? snippet('footer') ?>
+    <div class="contributors">
+      <ul>
+        <? $contributors = page('contributors') ?>
+        <?= $contributors->title() ?>
+        <? foreach(page('contributors')->children() as $contributor): ?>
+          <li><a href="<?= $contributor->url() ?>"><?= $contributor->title() ?></a></li>
+        <? endforeach ?>
+      </ul>
+
+      <div class="about">
+        <? $about = page('about') ?>
+        <?= $about->title() ?>
+        <?= $about->text()->kirbytext() ?>
+      </div>
+
+    </div>
+  </nav>
+
+<?php snippet('footer') ?>
